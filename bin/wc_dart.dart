28dc3d0a2cb8210c264128a8552b1904cee8d358
Samuel Marques <samuel.s.marques@protonmail.com>
2023-05-18 14:13:41 -0300

    add arguments and implement them

--- bin/wc_dart.dart
+++ bin/wc_dart.dart
@@ -1,5 +1,68 @@
+import 'dart:io';
+
+import 'package:args/args.dart';
 import 'package:wc_dart/wc_dart.dart' as wc_dart;
 
-void main(List<String> arguments) {
-  print('Hello world: ${wc_dart.calculate()}!');
+void main(List<String> arguments) async {
+  final ArgParser parser = ArgParser();
+
+  parser.addFlag('bytes', abbr: 'c');
+  parser.addFlag('lines', abbr: 'l');
+  parser.addFlag('words', abbr: 'w');
+  parser.addFlag('chars', abbr: 'm');
+
+  ArgResults argResults = parser.parse(arguments);
+
+  final List<String> paths = argResults.rest;
+  final bool allCommands = !argResults.wasParsed('bytes') && !argResults.wasParsed('lines') && !argResults.wasParsed('chars') && !argResults.wasParsed('words');
+
+  await wc(
+    paths,
+    countBytes: allCommands ? true : argResults.wasParsed('bytes'),
+    countLines: allCommands ? true : argResults.wasParsed('lines'),
+    countChars: allCommands ? true : argResults.wasParsed('chars'),
+    countWords: allCommands ? true : argResults.wasParsed('words'),
+  );
+}
+
+String padRight(String value) {
+  return '$value ';
+}
+
+Future<void> wc(
+  List<String> paths, {
+  bool countBytes = false,
+  bool countLines = false,
+  bool countWords = false,
+  bool countChars = false,
+}) async {
+  if (paths.isEmpty) {
+    stderr.writeln('Error: no file found.');
+  } else {
+    for (int index = 0; index < paths.length; index++) {
+      final File file = File(paths[index]);
+
+      if (countBytes) {
+        final int bytes = await wc_dart.countBytes(file);
+        stdout.write(padRight(bytes.toString()));
+      }
+
+      if (countLines) {
+        final int lines = await wc_dart.countLines(file);
+        stdout.write(padRight(lines.toString()));
+      }
+
+      if (countWords) {
+        final int words = await wc_dart.countWords(file);
+        stdout.write(padRight(words.toString()));
+      }
+
+      if (countChars) {
+        final int chars = await wc_dart.countChars(file);
+        stdout.write(padRight(chars.toString()));
+      }
+
+      stdout.write(padRight(file.path));
+    }
+  }
 }
