6a3e61cbd39904e588ba3b48560759f2c78827a9
Samuel Marques <samuel.s.marques@protonmail.com>
2023-05-18 14:13:26 -0300

    add countWords, countChars, countLines, countBytes

--- lib/wc_dart.dart
+++ lib/wc_dart.dart
@@ -1,3 +1,22 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:io';
+import 'dart:typed_data';
+
+Future<int> countBytes(File file) async {
+  final Uint8List bytes = await file.readAsBytes();
+  return bytes.length;
+}
+
+Future<int> countLines(File file) async {
+  final List<String> lines = await file.readAsLines();
+  return lines.length;
+}
+
+Future<int> countChars(File file) async {
+  final String text = await file.readAsString();
+  return text.length;
+}
+
+Future<int> countWords(File file) async {
+  final String text = await file.readAsString();
+  return RegExp(r'[\w-]+').allMatches(text).length;
 }
