129660638a37bfb8e0cb6a4783c38503c34d5a78
Samuel Marques <samuel.s.marques@protonmail.com>
2023-05-19 17:37:24 -0300

    add simple wc_dart_test

--- test/wc_dart_test.dart
+++ test/wc_dart_test.dart
@@ -1,8 +1,17 @@
-import 'package:wc_dart/wc_dart.dart';
+import 'package:test_process/test_process.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  group('wc-tool', () {
+    test('should count lines, words, chars and bytes correctly', () async {
+      final int expectedLines = 2;
+      final int expectedWords = 7;
+      final int expectedChars = 35;
+      final int expectedBytes = 37;
+
+      TestProcess process = await TestProcess.start('dart', ['run', 'bin/wc_dart.dart', 'test/test1.txt']);
+      expectLater(await process.stdout.next, equals('$expectedLines $expectedWords $expectedChars $expectedBytes test/test1.txt '));
+      await process.shouldExit(0);
+    });
   });
 }
